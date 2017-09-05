b77f3e6e64c0a7a33e74e944f4c4ba42c601aeeb
Maciej Palmowski <m.palmowski@spiders.agency>
2017-09-05 20:50:15 +0200

    jquery fixes

--- scrollTo.js
+++ scrollTo.js
@@ -3,26 +3,52 @@ window.$ = $;
 $('.scrollto').on( 'click', function( e ){
     e.preventDefault();
 
+    // id, class or DOM element to scroll
     var scroll_to = $(this).data( 'scroll-to' );
+
+    // offset
     var offset = $(this).data( 'offset' );
+
+    // easing 
     var easing = $(this).data( 'easing' );
+
+    // what is base of scroll - default is home, body
+    var base = $(this).data( 'base' );
+
+    // how long animaton takes
+    var duration = $(this).data( 'duration' );
 
     if ( offset === undefined ) {
         offset = 0;
     }
+
     if ( easing === undefined ) {
         easing = 'swing';
     }
 
-    var scrollTop = $( scroll_to ).offset().top - offset;
+    if ( base === undefined ) {
+        base = 'html, body';
+    }
 
-    if( offset !== undefined && scroll_to !== undefined && easing !== undefined ) {
-        $( 'html,body' ).animate(
-            {
-                scrollTop: scrollTop 
-            },
-            easing
-        );
+    if ( duration === undefined ) {
+        duration = 400;
+    }
+    
+    // check if element exists
+    if ( $( scroll_to ).length ) {
+
+        var scrollTop = $( scroll_to ).offset().top - offset;
+
+        
+        if( offset !== undefined && scroll_to !== undefined && easing !== undefined ) {
+            $( base ).animate(
+                {
+                    'scrollTop': scrollTop 
+                },
+                duration,
+                easing
+            );
+        }
     }
     
 });
